49d868751800b60133c4e85dd2630d3462cb5950
varun530 <vjindal067@gmail.com>
2025-09-29 14:35:36 +0530

    Added varun1.c program

---
+++ varun1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,sum;
+
+printf("enter first number:");
+scanf("%d",&num1);
+
+printf("enter second number:");
+scanf("%d",&num2);
+
+sum=num1+num2;
+printf("sum=%d \n",sum);
+
+return 0;
+}
