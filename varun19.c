279d61e3f368b8068409c3cb69b652267b387d4b
varun530 <vjindal067@gmail.com>
2025-11-11 23:54:45 +0530

    Added varun19.c program

---
+++ varun19.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+ {
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a + b > c && a + c > b && b + c > a) {
+        if (a == b && b == c) {
+            printf("Equilateral");
+        }
+        else if (a == b || b == c || a == c) {
+            printf("Isosceles");
+        }
+        else {
+            printf("Scalene");
+        }
+    } else {
+        printf("Not a valid triangle");
+    }
+
+    return 0;
+}
