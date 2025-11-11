bce3779b278a3ac8489abfae526f18084a6e6008
varun530 <vjindal067@gmail.com>
2025-11-11 23:30:06 +0530

    Added varun12.c program

---
+++ varun12.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int num;
+	printf("Enter an integer: ");
+	scanf("%d", &num);
+if (num > 0) {
+	printf("Positive\n");
+} else {
+if (num < 0) {
+	printf("Negative\n");
+     } else {
+	printf("Zero\n");
+   }
+}
+
+return 0;
+}
