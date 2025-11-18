24861c45d91a65c6289c826ba6b09245801a8866
varun530 <vjindal067@gmail.com>
2025-11-18 12:28:40 +0530

    Added varun27.c program

---
+++ varun27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i, sum = 0, odd = 1;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) 
+    {
+        sum += odd;
+        odd += 2;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
