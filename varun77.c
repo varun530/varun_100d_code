cab82ff0e4cd745c1f3c41f7b4b4758030eab4d2
varun530 <vjindal067@gmail.com>
2025-12-02 19:10:55 +0530

    Added varun77.c program

---
+++ varun77.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    if (r != c) {
+        printf("False");
+        return 0;
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = i + 1; j < r; j++) {
+            if (a[i][i] == a[j][j]) {
+                printf("False");
+                return 0;
+            }
+        }
+    }
+
+    printf("True");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
