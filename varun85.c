a6de82eacd8cfe5b38d1a37735af66329d160385
varun530 <vjindal067@gmail.com>
2025-12-02 23:28:41 +0530

    Added varun85.c program

---
+++ varun85.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int len = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    while (str[len] != '\0' && str[len] != '\n') {
+        len++;
+    }
+
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
