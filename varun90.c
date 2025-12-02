7aea5a1f366e7db3bd011d3a373185d451aca4f4
varun530 <vjindal067@gmail.com>
2025-12-02 23:31:59 +0530

    Added varun90.c program

---
+++ varun90.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;
+        }
+    }
+
+    printf("%s", str);
+    return 0;
+}
