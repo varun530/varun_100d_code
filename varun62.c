a3445cc8e27c9c6cc6dc1db514201ed3752e18e2
varun530 <vjindal067@gmail.com>
2025-12-02 18:53:43 +0530

    Added varun62.c program

---
+++ varun62.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i, temp;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n/2; i++) 
+    {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    for(i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
