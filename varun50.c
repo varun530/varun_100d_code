04b7f094be567bf6220daee13c855a5f7e6f56f5
varun530 <vjindal067@gmail.com>
2025-12-02 18:47:35 +0530

    Added varun50.c program

---
+++ varun50.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() 
+{
+    int i, j, space;
+
+    for(i = 1; i <= 5; i++) 
+    {
+        for(space = 1; space < i; space++) 
+        {
+            printf(" ");
+        }
+
+        for(j = 5; j >= i; j--) 
+        {
+            printf("*");
+        }
+
+        printf("\n"); 
+    }
+
+    return 0;
+}
