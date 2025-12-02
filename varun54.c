826fda9577caa01d3181b9db438415e7776ee959
varun530 <vjindal067@gmail.com>
2025-12-02 18:50:02 +0530

    Added varun54.c program

---
+++ varun54.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main() 
+{
+    int i, j, space;
+
+    for(i = 1; i <= 4; i++) 
+    {
+        for(space = i; space < 4; space++) 
+        {
+            printf(" ");
+        }
+        for(j = 1; j <= (2*i - 1); j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for(i = 3; i >= 1; i--) 
+    {
+        for(space = 4; space > i; space--) 
+        {
+            printf(" ");
+        }
+        for(j = 1; j <= (2*i - 1); j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
