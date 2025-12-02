a5df14340c0a09afa299226053cc5c694343310b
varun530 <vjindal067@gmail.com>
2025-12-02 23:29:32 +0530

    Added varun87.c program

---
+++ varun87.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int spaces = 0, digits = 0, special = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ')
+            spaces++;
+        else if (str[i] >= '0' && str[i] <= '9')
+            digits++;
+        else if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+            continue;  // ignore alphabets
+        else if (str[i] != '\n')
+            special++;
+    }
+
+    printf("Spaces=%d, Digits=%d, Special=%d", spaces, digits, special);
+    return 0;
+}
