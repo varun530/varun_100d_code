b02bc34a113270c5ccb1dfbc543c380379ffdc28
varun530 <vjindal067@gmail.com>
2025-12-02 18:51:11 +0530

    Added varun57.c program

---
+++ varun57.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() 
+{
+    int n, i, sum = 0;
+    int arr[100];
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i];  
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
