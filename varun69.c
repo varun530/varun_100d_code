13c270173c997c79e8aabe8d6c77421eb80253ce
varun530 <vjindal067@gmail.com>
2025-12-02 18:59:22 +0530

    Added varun69.c program

---
+++ varun69.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max1 = -999999, max2 = -999999;
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] > max1) {
+            max2 = max1;
+            max1 = arr[i];
+        }
+        else if(arr[i] > max2 && arr[i] != max1) {
+            max2 = arr[i];
+        }
+    }
+
+    printf("%d\n", max2);
+
+    return 0;
+}
