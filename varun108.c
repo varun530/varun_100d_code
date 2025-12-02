cc669f849723327bf393477b1ebdf7f0d994f87f
varun530 <vjindal067@gmail.com>
2025-12-02 23:46:15 +0530

    Added varun108.c program

---
+++ varun108.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+    for(int i = 0; i < n; i++) 
+    {
+        scanf("%d", &nums[i]);
+    }
+
+    long long prefix[n], suffix[n], answer[n];
+
+    prefix[0] = 1;
+    for(int i = 1; i < n; i++) 
+    {
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+    }
+
+    suffix[n - 1] = 1;
+    for(int i = n - 2; i >= 0; i--) 
+    {
+        suffix[i] = suffix[i + 1] * nums[i + 1];
+    }
+
+    for(int i = 0; i < n; i++) 
+    {
+        answer[i] = prefix[i] * suffix[i];
+    }
+
+    printf("[");
+    for(int i = 0; i < n; i++) 
+    {
+        printf("%lld", answer[i]);
+        if(i != n - 1) printf(",");
+    }
+    printf("]\n");
+
+    return 0;
+}
