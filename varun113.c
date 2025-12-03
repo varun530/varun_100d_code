244c2a54426be48837c7ff3e5aeeaf1ab33a2e0b
varun530 <vjindal067@gmail.com>
2025-12-03 21:35:50 +0530

    Added varun113.c program

---
+++ varun113.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, k;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter value of k: ");
+    scanf("%d", &k);
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("%d\n", arr[k - 1]); 
+    return 0;
+}
