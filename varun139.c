d5507c1a6c8b0688d64817454708d9917a326c41
varun530 <vjindal067@gmail.com>
2025-12-03 21:52:26 +0530

    Added varun139.c program

---
+++ varun139.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum Status {
+    SUCCESS,
+    FAILURE,
+    TIMEOUT
+};
+
+int main() {
+    printf("SUCCESS = %d\n", SUCCESS);
+    printf("FAILURE = %d\n", FAILURE);
+    printf("TIMEOUT = %d\n", TIMEOUT);
+
+    return 0;
+}
