0a690a734c1f3a9a9df9bf60fdae448bc05e6076
varun530 <vjindal067@gmail.com>
2025-12-03 21:50:04 +0530

    Added varun135.c program

---
+++ varun135.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum Letters {
+    A = 10,
+    B,
+    C
+};
+
+int main() {
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+    printf("C = %d\n", C);
+
+    return 0;
+}
