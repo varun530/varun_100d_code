618d70853378044dca32a5c18e7498204b816e55
varun530 <vjindal067@gmail.com>
2025-12-03 21:37:07 +0530

    Added varun116.c program

---
+++ varun116.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, target;
+    scanf("%d", &n);
+
+    int nums[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    scanf("%d", &target);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(nums[i] + nums[j] == target) {
+                printf("%d %d\n", i, j);
+                return 0; 
+            }
+        }
+    }
+
+    printf("-1 -1\n");
+    return 0;
+}
